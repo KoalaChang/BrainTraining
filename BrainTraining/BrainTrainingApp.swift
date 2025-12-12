222a5385e9d8eb2ca534043dfa21ca5680197669
KoalaChang <nobelesse799@gmail.com>
2025-12-12 14:38:41 +0800

    Initial Commit

---
+++ BrainTraining/BrainTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingApp.swift
+//  BrainTraining
+//
+//  Created by 張宇涵 on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainTrainingTests/BrainTrainingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingTests.swift
+//  BrainTrainingTests
+//
+//  Created by 張宇涵 on 2025/12/12.
+//
+
+import Testing
+@testable import BrainTraining
+
+struct BrainTrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
